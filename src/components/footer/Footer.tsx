835bd3f4c89fcb942a9f6a09e6e4b2cec0c6f2d6
proshanto7 <proshantosarker153@gmail.com>
2025-11-29 01:41:01 +0600

    full project responsive done

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -61,90 +61,88 @@ function Footer() {
     <footer className="bg-primary py-4">
       <Container>
         <div className="text-center">
-          <h2 className="text-[80px] text-white font-anek font-bold mb-3">
+          <h2 className="lg:text-[80px] text-5xl text-white font-anek font-bold mb-3">
             ভর্তি চলছে
           </h2>
-          <p className="text-[20px] font-anek text-white font-bold mb-6">
+          <p className="lg:text-[20px] text-[16px] font-anek text-white font-bold mb-6">
             আমাদের অনলাইন অথবা অফলাইন কোর্সে এনরোল করে ফেলুন
           </p>
 
-          <div className="flex gap-6 justify-center pb-[118px]">
+          <div className="flex gap-6 justify-center pb-7 lg:pb-[118px]">
             <Button
               title="সকল কোর্স দেখুন"
-              className="border border-white py-2.5 px-15 rounded-[15px] text-white font-anek text-[20px] font-bold"
+              className="border border-white py-2.5 px-15 rounded-[15px] text-white font-anek text-[14px] lg:text-[20px] font-bold"
             />
             <Button
               title="আমাদের সেমিনারে যোগ দিন"
-              className="border border-white py-2.5 px-[15px] rounded-[15px] text-white font-anek text-[20px] font-bold"
+              className="border border-white py-2.5 px-[15px] rounded-[15px] text-white font-anek text-[14px] lg:text-[20px] font-bold"
             />
           </div>
         </div>
 
-        <div className="flex justify-between  pb-15">
-          <div>
-            <h5 className="text-2xl font-semibold font-anek text-white mb-7">
-              আমাদের ঠিকানা
-            </h5>
-            <ul>
-              <li className="flex gap-3 text-white font-anek font-medium text-2xl mb-3">
-                <FaMapMarkerAlt />
-                <span>২৮ / ২, মেহের প্লাজা, ধানমন্ডি, ঢাকা</span>
-              </li>
-              <li className="flex gap-3 text-white font-anek font-medium text-2xl mb-3">
-                <FaPhone /> <span>+৮৮ ০১৭১৫৫৫৭৮৯৬</span>
-              </li>
-              <li className="flex gap-3 text-white font-anek font-medium text-2xl mb-3">
-                <MdEmail /> <span>info@codeduniya.com</span>
-              </li>
-            </ul>
-          </div>
-
-          <div className="flex gap-10">
-            <div>
-              <h5 className="text-2xl font-semibold font-anek text-white mb-7">
-                গুরুত্বপূর্ণ লিংক
+        <div className="flex lg:justify-between lg:flex-row flex-col pb-5  lg:pb-15">
+          <div className="lg:w-3/4 w-full flex flex-col sm:flex-row sm:gap-0 gap-4">
+            <div className="w-2/4">
+              <h5 className="lg:text-2xl  text-lg font-semibold font-anek text-white mb-4 lg:mb-7">
+                আমাদের ঠিকানা
               </h5>
               <ul>
-                {footerLinks.map((item) => (
-                  <ListItem
-                    key={item.id}
-                    items={item}
-                    className="font-anek text-[20px] font-normal text-white mb-3"
-                  />
-                ))}
+                <li className="flex gap-3 text-white font-anek font-medium text-[14px] lg:text-2xl mb-1.5 lg:mb-3">
+                  <FaMapMarkerAlt />
+                  <span>২৮ / ২, মেহের প্লাজা, ধানমন্ডি, ঢাকা</span>
+                </li>
+                <li className="flex gap-3 text-white font-anek font-medium text-[14px] lg:text-2xl mb-1.5 lg:mb-3">
+                  <FaPhone /> <span>+৮৮ ০১৭১৫৫৫৭৮৯৬</span>
+                </li>
+                <li className="flex gap-3 text-white font-anek font-medium text-[14px] lg:text-2xl mb-1.5 lg:mb-3">
+                  <MdEmail /> <span>info@codeduniya.com</span>
+                </li>
               </ul>
             </div>
-            <div>
-              <h5 className="text-2xl font-semibold font-anek text-white mb-7">
-                জনপ্রিয় কোর্সগুল
-              </h5>
-              <ul>
-                {footerCourses.map((item) => (
-                  <ListItem
-                    key={item.id}
-                    items={item}
-                    className="font-anek text-[20px] font-normal text-white mb-3"
-                  />
-                ))}
-              </ul>
+
+            <div className="flex  gap-10">
+              <div>
+                <h5 className="lg:text-2xl  text-lg font-semibold font-anek text-white mb-4 lg:mb-7">
+                  গুরুত্বপূর্ণ লিংক
+                </h5>
+                <ul>
+                  {footerLinks.map((item) => (
+                    <ListItem
+                      key={item.id}
+                      items={item}
+                      className="font-anek text-[14px] lg:text-[20px] font-normal text-white mb-1.5 lg:mb-3"
+                    />
+                  ))}
+                </ul>
+              </div>
+              <div>
+                <h5 className="lg:text-2xl  text-lg font-semibold font-anek text-white mb-4 lg:mb-7">
+                  জনপ্রিয় কোর্সগুল
+                </h5>
+                <ul>
+                  {footerCourses.map((item) => (
+                    <ListItem
+                      key={item.id}
+                      items={item}
+                      className="font-anek text-[14px] lg:text-[20px] font-normal text-white mb-1.5 lg:mb-3"
+                    />
+                  ))}
+                </ul>
+              </div>
             </div>
           </div>
 
-          <div>
-            <Image
-              imgUrl={FooterImg}
-              alt="footer img"
-              className="max-w-[377px]"
-            />
+          <div className="w-1/4 hidden lg:block">
+            <Image imgUrl={FooterImg} alt="footer img" className="  w-full" />
           </div>
         </div>
 
         <div className="text-center text-white font-anek">
-          <h1 className="font-bold text-[80px] mb-4">কোড দুনিয়া</h1>
-          <h5 className="font-normal text-4xl">
+          <h1 className="font-bold text-4xl lg:text-[80px] mb-4">কোড দুনিয়া</h1>
+          <h5 className="font-normal text-2xl lg:text-4xl">
             দেশের সর্ববৃহৎ আইটি ট্রেনিং প্লাটফর্ম
           </h5>
-          <ul className="flex justify-center gap-6 text-[40px] mt-7">
+          <ul className="flex justify-center gap-6 text-3xl lg:text-[40px] mt-7">
             <li>
               <a href="#">
                 <FaFacebook />
@@ -167,11 +165,9 @@ function Footer() {
             </li>
           </ul>
         </div>
-        <div className="flex justify-between text-white font-anek  text-[20px] font-normal mt-[131px]">
-
-<p>২০২৪ কপিরাইট  | মাহমুদ হাসান রামীম | সর্বসত্ত্ব সংরক্ষিত</p>
-<p>প্রাইভেসি পলিসি</p>
-
+        <div className="flex justify-between text-white font-anek text-[12px] lg:text-[20px] font-normal mt-6 lg:mt-[131px]">
+          <p>২০২৪ কপিরাইট | মাহমুদ হাসান রামীম | সর্বসত্ত্ব সংরক্ষিত</p>
+          <p>প্রাইভেসি পলিসি</p>
         </div>
       </Container>
     </footer>
